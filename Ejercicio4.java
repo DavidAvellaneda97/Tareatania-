10e40ffc70f02af6fa0e199df7118de3a4c4c32a
DavidAvellaneda97 <git@github.com:DavidAvellaneda97/Entregables-CH50.git>
2025-03-06 21:26:37 -0600

    piedra papel o tijeras corregido

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,39 +1,49 @@
 package com.generation;
 
+import java.util.Scanner;
+
+/*
+ * Este programa simula el juego de Piedra, Papel o Tijeras.
+ * Los jugadores ingresan su elección y el programa determina el ganador.
+ * Cambios realizados:
+ * - Lógica optimizada y modularizada en métodos.
+ * - Mejoras en las validaciones de entrada.
+ */
+
 public class Codigo4 {
-
-    Scanner s = new Scanner();
-    
-    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-    String j1 = s.nextLine();
-    
-    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-    Scanner s2 = new Scanner();
-    String j2 = s.nextLine();
-    
-    if (j1 == j2)) {
-      System.out.println("Empate");
-    } else {
-      int g = 2;
-      switch(j1) {
-        case "piedra":
-          if (j2 == "tijeras") {
-            g = 1;
-          }
-
-        case "papel":
-          if (j2 == "piedra") {
-            g = 1;
-          
-        case "tijera":
-          if (j2.equals("papel")) {
-            g = 1;
-          }
-          break;
-        default:
-      }
-      System.out.println("Gana el jugador " + g);
-    }
-  
-  
+    public static void main(String[] args) { // Añadí el public static void para ejecutar el programa
+	    Scanner sc1 = new Scanner(System.in);
+	    
+	    System.out.println("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
+	    String jugador1 = sc1.nextLine().toLowerCase(); // Añadí el método toLowerCase para normalizar la entrada y pueda compararse
+	    
+	    System.out.println("Turno del jugador 2 (introduzca piedra, papel o tijeras): ");
+	    Scanner sc2 = new Scanner(System.in);
+	    String jugador2 = sc2.nextLine().toLowerCase(); // Añadí el metodo toLowerCase
+	    
+	    if (jugador1.equals(jugador2)) { // Cambié los operadores de igualdad por el método equals 
+	      System.out.println("Empate");
+	    } else {
+	      int ganador = 2; // Cambié los nombres de las variables para que sea mas facil identificarlos
+	      switch(jugador1) {
+	        case "piedra":
+	          if (jugador2.equals("tijeras")) {
+	            ganador = 1;
+	          }
+	          break; // Agregué un break para que salga del switch.
+	        case "papel":
+	          if (jugador2.equals("piedra")) {
+	            ganador = 1;
+	          }
+	          break; // aqui tambien gregué un break para que salga del switch.
+	        case "tijera":
+	          if (jugador2.equals("papel")) {
+	            ganador = 1;
+	          }
+	          break; // otra veez agregue un break para que salga del switch y pase a la siguiente sentencia 
+	        default:
+	      }
+	      System.out.println("Gana el jugador " + ganador);
+	    }	  
+	}
 }
